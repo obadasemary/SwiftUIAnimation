247998d5e6f07abbe3f05eb344e83e2f6a09e02d
Abdelrahman Mohamed <obada.semary@gmail.com>
2020-08-30 17:45:18 +0300

    Creating a Loading Indicator Using RotationEffect
    Creating a Loading Indicator Using RotationEffect
    
    Co-Authored-By: Abdelrahman Mohamed <obada.semary@gmail.com>

--- SwiftUIAnimation/ContentView.swift
+++ SwiftUIAnimation/ContentView.swift
@@ -22,73 +22,111 @@ struct ContentView: View {
     @State private var redHeartColorChanged = false
     @State private var redHeartSizeChanged = false
     
+    @State private var isOverlayLoading = false
+    @State private var isLoading = false
+    
     var body: some View {
         
-        HStack {
+        VStack(spacing: 20) {
             
-            ZStack {
-                Circle()
-                    .frame(width: 100, height: 100)
-                    .foregroundColor(pinkCircleColorChanged ? Color(.systemGray5) : .pink)
-                    .animation(.spring(response: 0.3, dampingFraction: 0.3, blendDuration: 0.3))
-
-                Image(systemName: "heart.fill")
-                    .foregroundColor(pinkHeartColorChanged ? .pink : .white)
-                    .font(.system(size: 50))
-                    .animation(nil) // Cancel the animation from here
-                    .scaleEffect(pinkHeartSizeChanged ? 1.0 : 0.5)
-                    .animation(.spring(response: 0.3, dampingFraction: 0.3, blendDuration: 0.3))
-            }
-            .onTapGesture {
-                self.pinkCircleColorChanged.toggle()
-                self.pinkHeartColorChanged.toggle()
-                self.pinkHeartSizeChanged.toggle()
-            }
-            
-            ZStack {
+            HStack(spacing: 20) {
                 
-                // Explicit Animations
-                
-                Circle()
-                    .frame(width: 100, height: 100)
-                    .foregroundColor(yellowCircleColorChanged ? Color(.systemGray5) : .yellow)
-                
-                Image(systemName: "heart.fill")
-                    .foregroundColor(yellowHeartColorChanged ? .yellow : .white)
-                    .font(.system(size: 50))
-                    .scaleEffect(yellowHeartSizeChanged ? 1.0 : 0.5)
-            }
-            .onTapGesture {
-                withAnimation(.spring(response: 0.3, dampingFraction: 0.3, blendDuration: 0.3)) {
-                    self.yellowCircleColorChanged.toggle()
-                    self.yellowHeartColorChanged.toggle()
+                ZStack {
+                    
+                    Circle()
+                        .stroke(Color(.systemGray5), lineWidth: 14)
+                        .frame(width: 100, height: 100)
+                    
+                    Circle()
+                        .trim(from: 0, to: 0.2)
+                        .stroke(Color.orange, lineWidth: 7)
+                        .frame(width: 100, height: 100)
+                        .rotationEffect(Angle(degrees: isOverlayLoading ? 360: 0))
+                        .animation(Animation.linear(duration: 1).repeatForever(autoreverses: false))
+                        .onAppear() {
+                            self.isOverlayLoading = true
+                        }
                 }
-                self.yellowHeartSizeChanged.toggle()
-//                withAnimation(.default) {
-//                    self.yellowCircleColorChanged.toggle()
-//                    self.yellowHeartColorChanged.toggle()
-//                    self.yellowHeartSizeChanged.toggle()
-//                }
-            }
-            
-            ZStack {
-                
-                // Implicit Animations
                 
                 Circle()
+                    .trim(from: 0, to: 0.7)
+                    .stroke(Color.orange, lineWidth: 5)
                     .frame(width: 100, height: 100)
-                    .foregroundColor(redCircleColorChanged ? Color(.systemGray5) : .red)
-                
-                Image(systemName: "heart.fill")
-                    .foregroundColor(redHeartColorChanged ? .red : .white)
-                    .font(.system(size: 50))
-                    .scaleEffect(redHeartSizeChanged ? 1.0 : 0.5)
+                    .rotationEffect(Angle(degrees: isLoading ? 360 : 0))
+//                    .animation(Animation.default.repeatForever(autoreverses: false))
+                    .animation(Animation.linear(duration: 1).repeatForever(autoreverses: false))
+                    .onAppear() {
+                        self.isLoading = true
+                    }
             }
-            .animation(.spring(response: 0.3, dampingFraction: 0.3, blendDuration: 0.3))
-            .onTapGesture {
-                self.redCircleColorChanged.toggle()
-                self.redHeartColorChanged.toggle()
-                self.redHeartSizeChanged.toggle()
+            
+            HStack {
+                
+                ZStack {
+                    
+                    Circle()
+                        .frame(width: 100, height: 100)
+                        .foregroundColor(pinkCircleColorChanged ? Color(.systemGray5) : .pink)
+                        .animation(.spring(response: 0.3, dampingFraction: 0.3, blendDuration: 0.3))
+                    
+                    Image(systemName: "heart.fill")
+                        .foregroundColor(pinkHeartColorChanged ? .pink : .white)
+                        .font(.system(size: 50))
+                        .animation(nil) // Cancel the animation from here
+                        .scaleEffect(pinkHeartSizeChanged ? 1.0 : 0.5)
+                        .animation(.spring(response: 0.3, dampingFraction: 0.3, blendDuration: 0.3))
+                }
+                .onTapGesture {
+                    self.pinkCircleColorChanged.toggle()
+                    self.pinkHeartColorChanged.toggle()
+                    self.pinkHeartSizeChanged.toggle()
+                }
+                
+                ZStack {
+                    
+                    // Explicit Animations
+                    
+                    Circle()
+                        .frame(width: 100, height: 100)
+                        .foregroundColor(yellowCircleColorChanged ? Color(.systemGray5) : .yellow)
+                    
+                    Image(systemName: "heart.fill")
+                        .foregroundColor(yellowHeartColorChanged ? .yellow : .white)
+                        .font(.system(size: 50))
+                        .scaleEffect(yellowHeartSizeChanged ? 1.0 : 0.5)
+                }
+                .onTapGesture {
+                    withAnimation(.spring(response: 0.3, dampingFraction: 0.3, blendDuration: 0.3)) {
+                        self.yellowCircleColorChanged.toggle()
+                        self.yellowHeartColorChanged.toggle()
+                    }
+                    self.yellowHeartSizeChanged.toggle()
+                    //                withAnimation(.default) {
+                    //                    self.yellowCircleColorChanged.toggle()
+                    //                    self.yellowHeartColorChanged.toggle()
+                    //                    self.yellowHeartSizeChanged.toggle()
+                    //                }
+                }
+                
+                ZStack {
+                    
+                    // Implicit Animations
+                    
+                    Circle()
+                        .frame(width: 100, height: 100)
+                        .foregroundColor(redCircleColorChanged ? Color(.systemGray5) : .red)
+                    
+                    Image(systemName: "heart.fill")
+                        .foregroundColor(redHeartColorChanged ? .red : .white)
+                        .font(.system(size: 50))
+                        .scaleEffect(redHeartSizeChanged ? 1.0 : 0.5)
+                }
+                .animation(.spring(response: 0.3, dampingFraction: 0.3, blendDuration: 0.3))
+                .onTapGesture {
+                    self.redCircleColorChanged.toggle()
+                    self.redHeartColorChanged.toggle()
+                    self.redHeartSizeChanged.toggle()
+                }
             }
         }
     }
