3560c2782ed96467ac89c34d9effa81b5a0e6b72
Abdelrahman Mohamed <obada.semary@gmail.com>
2020-08-31 00:47:28 +0300

    Transforming a Rectangle into Circle
    Co-Authored-By: Abdelrahman Mohamed <obada.semary@gmail.com>

--- SwiftUIAnimation/ContentView.swift
+++ SwiftUIAnimation/ContentView.swift
@@ -18,6 +18,9 @@ struct ContentView: View {
     @State private var isOverlayLoading = false
     @State private var isLoading = false
     
+    @State private var recordBegin = false
+    @State private var recording = false
+    
     @State private var pinkCircleColorChanged = false
     @State private var pinkHeartColorChanged = false
     @State private var pinkHeartSizeChanged = false
@@ -152,6 +155,38 @@ struct ContentView: View {
                     .onAppear() {
                         self.isLoading = true
                     }
+            }
+            
+            Group {
+                
+                ZStack {
+                    
+                    RoundedRectangle(cornerRadius: recordBegin ? 30 : 5)
+                        .frame(width: recordBegin ? 60 : 250, height: 60)
+                        .foregroundColor(recordBegin ? .red : .green)
+                    .overlay(
+                        Image(systemName: "mic.fill")
+                            .font(.system(.title))
+                            .foregroundColor(.white)
+                            .scaleEffect(recording ? 0.7 : 1)
+                    )
+                    
+                    RoundedRectangle(cornerRadius: recordBegin ? 35 : 10)
+                        .trim(from: 0, to: recordBegin ? 0 : 1)
+                        .stroke(lineWidth: 5)
+                        .frame(width: recordBegin ? 70 : 260, height: 70)
+                        .foregroundColor(.green)
+                }
+                .onTapGesture {
+                    
+                    withAnimation(Animation.spring()) {
+                        self.recordBegin.toggle()
+                    }
+                    
+                    withAnimation(Animation.spring().repeatForever().delay(0.5)) {
+                        self.recording.toggle()
+                    }
+                }
             }
             
             HStack {
